58145c450b00de413bcdcf2b352d567012b37308
Yiming Yang <yang.yihming@gmail.com>
2020-06-19 00:40:19 -0400

    fix bug

--- demuxEM/__init__.py
+++ demuxEM/__init__.py
@@ -16,5 +16,5 @@ from importlib_metadata import version, PackageNotFoundError
 try:
     __version__ = version(__name__)
     del version
-except PackagteNotFoundError:
+except PackageNotFoundError:
     pass
